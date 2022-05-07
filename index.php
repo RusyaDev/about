925b3070b9c61b5b09666d0ac4dc0a6f580cbf10
RusyaDev <104848055+RusyaDev@users.noreply.github.com>
2022-05-07 04:46:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="https://abbosarts.netlify.app/home/img/abbosarts_avatar.png" type="image/png" sizes="16x16">
+  <title>🌏 Rusya - Offical Website ✅</title>
+  <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="noise"></div>
+  <div class="container for-padding">
+    <?php include('layout.php'); ?>
+    <div class="col-11">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="header-text">Assalomu Alaykum!</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="social">
+            <ul class="list">
+              <li class="list-item">
+                <a href="https://instagram.com/_web_devoloper_">instagram</a>
+              </li>
+              <li class="list-item li2">
+                <a href="https://t.me/MuchyMuchy">telegram</a>
+              </li>
+              <li class="list-item li3">
+                <a href="https://github.com/RusyaDev">github</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="text">
+        <div class="for-width">
+          <p class="main-txt">Men Bazarbaev Ruslan, Web dasturchi | Back End devoloper. Yoshim 18 da. Hozirda Nukus tumanida istiqomat qilaman. Web dasturlash sohasida 1yillik tajribam bor.</p>
+          <p class="main-txt">Men bo'sh paytlarimda Badiiy kitob o'qishga qiziqaman, ora-orada Video o'yinlar ham o'ynab turaman. <span>YouTube</span> platformasida videodarsliklar ko'rish sevimli mashg'ulotim. Kelajakda katta maqsadlar qo'ygan va ularni birin ketin amalga oshirib borayotgan oddiy yigitlardan biriman.</p>
+        </div>
+      </div>
+      <div class="copy">
+        &copy;Ruslan Bazarbaev 2022
+      </div>
+    </div>
+  </div>
+</body>
+</html>
